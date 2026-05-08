111f0d629824ece45a851c11a403d967e70cc9c8
yash <166590584+cynicalmindset@users.noreply.github.com>
2026-05-08 10:44:45 +0530

    Create 1800-ConcatenationOfConsecutiveBinaryNumbers

---
+++ 1800-ConcatenationOfConsecutiveBinaryNumbers/1800-ConcatenationOfConsecutiveBinaryNumbers.cpp
@@ -0,0 +1,15 @@
+// Last updated: 08/05/2026, 10:44:44
+class Solution {
+public:
+int M = 1e9+7;
+    int concatenatedBinary(int n) {
+        long result =0 ;
+        int digit = 0;
+        for(int num = 1; num<=n;num++){
+            if((num&(num-1))==0)
+                digit++;
+            result = ((result<<digit)%M + num)%M;
+        }
+        return result;
+    }
+};
