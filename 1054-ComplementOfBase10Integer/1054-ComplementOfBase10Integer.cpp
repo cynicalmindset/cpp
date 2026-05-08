96b2fcd79b179363f851bc1f87abebaade2b8dd4
yash <166590584+cynicalmindset@users.noreply.github.com>
2026-05-08 10:45:29 +0530

    Create 1054-ComplementOfBase10Integer

---
+++ 1054-ComplementOfBase10Integer/1054-ComplementOfBase10Integer.cpp
@@ -0,0 +1,18 @@
+// Last updated: 08/05/2026, 10:45:29
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int m = n;
+        int mask = 0;
+
+        if( n == 0) return 1;
+        while(m!=0){
+            mask = (mask << 1) | 1;
+            m = m >> 1;
+        }
+
+        int ans = (~n) & mask;
+        return ans;
+        
+    }
+};
