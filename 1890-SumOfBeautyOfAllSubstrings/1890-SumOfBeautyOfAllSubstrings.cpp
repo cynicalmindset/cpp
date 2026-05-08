3f6f00b2228e13402e6c4e27ebcb055194459c38
yash <166590584+cynicalmindset@users.noreply.github.com>
2026-05-08 10:44:39 +0530

    Create 1890-SumOfBeautyOfAllSubstrings

---
+++ 1890-SumOfBeautyOfAllSubstrings/1890-SumOfBeautyOfAllSubstrings.cpp
@@ -0,0 +1,26 @@
+// Last updated: 08/05/2026, 10:44:38
+class Solution {
+public:
+    int beauty(vector<int>&alpha){
+        int mf = -1;
+        int lf = 1e9;
+        for(int i = 0;i<26;i++){
+            mf=max(mf,alpha[i]);
+            if(alpha[i]>=1){
+                lf = min(lf,alpha[i]);
+            }
+        }
+        return mf-lf;
+    }
+    int beautySum(string s) {
+        int res =0;
+        for(int i = 0;i<s.size();i++){
+            vector<int>alpha(26,0);
+            for(int j=i;j<s.size();j++){
+                alpha[s[j]-'a']++;
+                res = res+beauty(alpha);
+            }
+        }
+        return res;
+    }
+};
