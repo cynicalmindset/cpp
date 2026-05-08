034c4f602c04aa308c91400565cb790dde3429df
yash <166590584+cynicalmindset@users.noreply.github.com>
2026-05-08 10:45:04 +0530

    Create 1552-BuildAnArrayWithStackOperations

---
+++ 1552-BuildAnArrayWithStackOperations/1552-BuildAnArrayWithStackOperations.cpp
@@ -0,0 +1,20 @@
+// Last updated: 08/05/2026, 10:45:04
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ops;
+        int idx = 0;
+
+        for (int num = 1; num <= n && idx < target.size(); num++) {
+            ops.push_back("Push");
+
+            if (num == target[idx]) {
+                idx++; 
+            } else {
+                ops.push_back("Pop"); 
+            }
+        }
+
+        return ops;
+    }
+};
